d4d9e8724416102533ae40bcfe69b94d9e9e4131
aleksandrustic <nickxone@mail.ru>
2023-03-31 10:44:01 +0100

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main() {
+  std::cout << "Hello SFML!" << std::endl;
+
   sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
